461ec7419f494a516bc971ba19a99af00a2a6e26
Jay9z <jay.cn@outlook.com>
2020-09-05 06:41:53 +0800

    feat: add init
    paper mentioned it initialize layers with normal(0,0.2), graddecay 0.0001.

--- networks.py
+++ networks.py
@@ -5,14 +5,7 @@ from paddle.fluid.dygraph import Sequential
 import paddle.fluid.dygraph.nn as nn
 #from paddle.fluid.Tensor import tensor
 import numpy as np
-
-w_param_attrs = fluid.ParamAttr(name="weight",
-                                initializer = fluid.initializer.NormalInitializer(loc=0.0,scale=0.2),
-                                learning_rate=0.0001,
-                                regularizer=fluid.regularizer.L2Decay(0.0001),
-                                trainable=True)
-weight_init = fluid.initializer.NormalInitializer(loc=0.0,scale=0.2)  
-weight_regularizer = fluid.regularizer.L2Decay(0.0001)
+from ops import *
 
 class ResnetGenerator(fluid.dygraph.Layer):
     def __init__(self, input_nc=3, output_nc=3, ngf=64, n_blocks=4, img_size=256, light=False):
@@ -29,8 +22,8 @@ class ResnetGenerator(fluid.dygraph.Layer):
         DownBlock = []
         DownBlock += [
                       ReflectionPad2d(3),
-                      Conv2D(num_channels=input_nc, num_filters=ngf, filter_size=7, stride=1, padding=0, bias_attr=False),
-                      InstanceNorm(ngf),
+                      Conv2D(num_channels=input_nc, num_filters=ngf, filter_size=7, stride=1, padding=0, bias_attr=True),
+                      InstanceNorm(ngf), #center,scale
                       ReLU()
                       ]
          
@@ -40,7 +33,7 @@ class ResnetGenerator(fluid.dygraph.Layer):
             mult = 2**i
             DownBlock += [
                           ReflectionPad2d(1),
-                          Conv2D(ngf * mult, ngf * mult * 2, filter_size=3, stride=2, padding=0, bias_attr=False),
+                          Conv2D(ngf * mult, ngf * mult * 2, filter_size=3, stride=2, padding=0, bias_attr=True),
                           InstanceNorm(ngf * mult * 2),
                           ReLU()                        
                           ]
@@ -48,33 +41,40 @@ class ResnetGenerator(fluid.dygraph.Layer):
         # ops for Encoder Bottleneck
         mult = 2**n_downsampling
         for i in range(n_blocks):
-            DownBlock += [ResnetBlock(ngf * mult, use_bias=False)]
+            DownBlock += [ResnetBlock(ngf * mult, use_bias=True)]
 
         # CAM of Generator
-        self.gap_fc = Linear(ngf * mult, 1,bias_attr=False)  
-        self.gmp_fc = Linear(ngf * mult, 1,bias_attr=False) 
-        self.conv1x1 = Conv2D(ngf * mult * 2, ngf * mult, filter_size=1, stride=1,bias_attr=False,act='relu') #bias=True
+        self.gap_fc = Linear(ngf * mult, 1,bias_attr=True)  
+        self.gmp_fc = Linear(ngf * mult, 1,bias_attr=True) 
+        self.conv1x1 = Conv2D(ngf * mult * 2, ngf * mult, filter_size=1, stride=1,bias_attr=True,act='relu') #bias=True
 
-        # ops for  Gamma, Beta block
-        if self.light:
-            FC = [Linear(ngf * mult, ngf * mult,bias_attr=False,act='relu'),
-                  #nn.ReLU(True),
-                  Linear(ngf * mult, ngf * mult,bias_attr=False,act='relu')
-                  #nn.ReLU(True)
-                  ]
-        else:
-            FC = [Linear(img_size // mult * img_size // mult * ngf * mult, ngf * mult, bias_attr=False,act='relu'),
-                  #nn.ReLU(True),
-                  Linear(ngf * mult, ngf * mult,bias_attr=False, act='relu')
-                  #nn.ReLU(True)
-                  ]
+        ## ops for  Gamma, Beta block
+        # if self.light:
+        #     FC = [Linear(ngf * mult, ngf * mult,bias_attr=False,act='relu'),
+        #           #nn.ReLU(True),
+        #           Linear(ngf * mult, ngf * mult,bias_attr=False,act='relu')
+        #           #nn.ReLU(True)
+        #           ]
             
-        self.gamma = Linear(ngf * mult, ngf * mult,bias_attr=False)  # FC256
-        self.beta = Linear(ngf * mult, ngf * mult,bias_attr=False) # FC256
+
+        # else:
+        #     FC = [Linear(img_size // mult * img_size // mult * ngf * mult, ngf * mult, bias_attr=False,act='relu'),
+        #           #nn.ReLU(True),
+        #           Linear(ngf * mult, ngf * mult,bias_attr=False, act='relu')
+        #           #nn.ReLU(True)
+        #           ]
+                
+            
+        # self.gamma = Linear(ngf * mult, ngf * mult,bias_attr=False)  # FC256
+        # self.beta = Linear(ngf * mult, ngf * mult,bias_attr=False) # FC256
+        if self.light:
+            self.mlp = MLP(ngf * mult,ngf * mult)
+        else:
+            self.mlp = MLP(img_size // mult * img_size // mult * ngf * mult, ngf * mult)
 
         # ops for Decoder Bottleneck
         for i in range(n_blocks):
-            setattr(self, 'UpBlock1_' + str(i+1), ResnetAdaILNBlock(ngf * mult, use_bias=False))
+            setattr(self, 'UpBlock1_' + str(i+1), ResnetAdaILNBlock(ngf * mult))
 
         # ops for Decoder Up-Sampling
         n_upsampling = n_downsampling
@@ -85,19 +85,19 @@ class ResnetGenerator(fluid.dygraph.Layer):
                          #fluid.layers.pad2d(1),
                          Upsample(),
                          ReflectionPad2d(1),
-                         Conv2D(ngf * mult, int(ngf * mult / 2), filter_size=3, stride=1, padding=0, bias_attr=False),
-                         LIN(int(ngf * mult / 2)),
+                         Conv2D(ngf * mult, ngf * mult // 2, filter_size=3, stride=1, padding=0, bias_attr=True),
+                         LIN(ngf * mult // 2),
                          ReLU()
                          ]
         
         UpBlock2 += [#fluid.layers.pad2d(3),
                      ReflectionPad2d(3),
-                     Conv2D(ngf, output_nc, filter_size=7, stride=1, padding=0, bias_attr=False,act='tanh')
+                     Conv2D(ngf, output_nc, filter_size=7, stride=1, padding=0, bias_attr=True,act='tanh')
                      #nn.Tanh()
                      ]
 
         self.DownBlock = Sequential(*DownBlock)
-        self.FC = Sequential(*FC)
+        #self.FC = Sequential(*FC)
         self.UpBlock2 = Sequential(*UpBlock2)
 
     def forward(self, input):  
@@ -130,125 +130,33 @@ class ResnetGenerator(fluid.dygraph.Layer):
         # Ouput_3
         heatmap = fluid.layers.reduce_sum(x, dim=1, keep_dim=True)  ## Attention map, #heatmap torch.Size([1, 1, 64, 64])
 
-        # alpha, beta
-        if self.light:
-            # 1/3,shape(N,256,64,64) -->(N,256,1,1)
-            x_ = fluid.layers.adaptive_pool2d(x, 1,pool_type='avg') # shape=(N,256,1,1)
-            x_ = fluid.layers.reshape(x_, shape=[x.shape[0], -1]) # shape=(N,256)  
-            # 2/3~3/3, x2 (N,256)-->(N,256) by FC256
-            x_ = self.FC(x_)
-        else:
-            # 1/3 (N,256,64,64)-->(N,256*64*64)
-            x_ = fluid.layers.reshape(x, shape=[x.shape[0], -1]) # shape=(N,256*64*64)
-            # 2/3 (N,64*64*256)-->(N,256), 2/3 (N,256)-->(N,256) by FC
-            x_ = self.FC(x_)
+        # # alpha, beta
+        # if self.light:
+        #     # 1/3,shape(N,256,64,64) -->(N,256,1,1)
+        #     x_ = fluid.layers.adaptive_pool2d(x, 1,pool_type='avg') # shape=(N,256,1,1)
+        #     x_ = fluid.layers.reshape(x_, shape=[x.shape[0], -1]) # shape=(N,256)  
+        #     # 2/3~3/3, x2 (N,256)-->(N,256) by FC256
+        #     x_ = self.FC(x_)
+        # else:
+        #     # 1/3 (N,256,64,64)-->(N,256*64*64)
+        #     x_ = fluid.layers.reshape(x, shape=[x.shape[0], -1]) # shape=(N,256*64*64)
+        #     # 2/3 (N,64*64*256)-->(N,256), 2/3 (N,256)-->(N,256) by FC
+        #     x_ = self.FC(x_)
         # (N,256)-->(N,256), parameters for AdaILN
-        gamma, beta = self.gamma(x_), self.beta(x_) # gamma torch.Size([N, 256]) beta torch.Size([N, 256])
+        #gamma, beta = self.gamma(x_), self.beta(x_) # gamma torch.Size([N, 256]) beta torch.Size([N, 256])
+        gamma,beta = self.mlp(x)
 
         # Decoder bottleneck, after CAM layer with (alpah,beta)
         for i in range(self.n_blocks):
             x = getattr(self, 'UpBlock1_' + str(i+1))(x, gamma, beta)
 
         # Decoder upsampling
-        out = self.UpBlock2(x)
+        x = self.UpBlock2(x)
 
         #out torch.Size([1, 3, 256, 256]) cam_logit torch.Size([1, 2])  heatmap torch.Size([1, 1, 64, 64])
-        return out, cam_logit, heatmap
-
-class ResnetBlock(fluid.dygraph.Layer):
-    def __init__(self, dim, use_bias):
-        super(ResnetBlock, self).__init__()
-        conv_block = []
-        conv_block += [ReflectionPad2d(1),
-                       Conv2D(dim, dim, filter_size=3, stride=1, padding=0, bias_attr=use_bias),
-                       InstanceNorm(dim),
-                       ReLU()                         
-                       ]
-
-        conv_block += [ReflectionPad2d(1),
-                       Conv2D(dim, dim, filter_size=3, stride=1, padding=0, bias_attr=use_bias),
-                       InstanceNorm(dim)
-                       ]
-
-        self.conv_block = Sequential(*conv_block)
-
-    def forward(self, x):
-        out = x + self.conv_block(x)
-        return out
+        return x, cam_logit, heatmap
 
 
-class ResnetAdaILNBlock(fluid.dygraph.Layer):
-    def __init__(self, dim, use_bias):
-        super(ResnetAdaILNBlock, self).__init__()
-        self.conv1 = Conv2D(dim, dim, filter_size=3, stride=1, padding=1, bias_attr=use_bias)
-        self.norm1 = AdaILN(dim)
-        self.relu1 = ReLU()
-        self.conv2 = Conv2D(dim, dim, filter_size=3, stride=1, padding=1, bias_attr=use_bias)
-        self.norm2 = AdaILN(dim)
-
-    def forward(self, x, gamma, beta):
-        out = self.conv1(x)
-        out = self.norm1(out, gamma, beta)
-        out= self.relu1(out)
-        out = self.conv2(out)
-        out = self.norm2(out, gamma, beta)
-
-        return out + x
-class AdaILN(fluid.dygraph.Layer):
-
-    def __init__(self, in_channels, eps=1e-5):
-        super(AdaILN, self).__init__()
-        self.eps = eps
-        self.rho =self.create_parameter((1, in_channels, 1, 1), dtype='float32', is_bias=True,
-        default_initializer=fluid.initializer.ConstantInitializer(0.9))
-
-    def var(self, input, dim):
-        mean = fluid.layers.reduce_mean(input, dim, keep_dim=True)
-        tmp = fluid.layers.reduce_mean((input - mean)**2, dim, keep_dim=True)
-        return tmp
-
-    def forward(self, input, gamma, beta):
-        in_mean, in_var = fluid.layers.reduce_mean(input, dim=[2,3], keep_dim=True), self.var(input, dim =[2,3])
-        ln_mean, ln_var = fluid.layers.reduce_mean(input, dim=[1,2,3], keep_dim=True), self.var(input, dim=[1,2,3])
-        out_in = (input - in_mean) / fluid.layers.sqrt(in_var + self.eps)
-        out_ln = (input - ln_mean) / fluid.layers.sqrt(ln_var + self.eps)
-        out = self.rho * out_in + (1 - self.rho)*out_ln
-        out=out*fluid.layers.unsqueeze(gamma,[2,3])+fluid.layers.unsqueeze(beta,[2,3])
-        return out
-
-class LIN(fluid.dygraph.Layer):
-
-    def __init__(self, in_channels, eps=1e-5):
-        super(LIN, self).__init__()
-        self.eps = eps
-        self.rho = self.create_parameter((1, in_channels, 1, 1), dtype='float32', is_bias=True,
-        default_initializer=fluid.initializer.ConstantInitializer(1.0))  # 0.0 -- LN, 1.0 --IN
-        self.gamma = self.create_parameter((1, in_channels, 1, 1), dtype='float32', is_bias=True,
-        default_initializer=fluid.initializer.ConstantInitializer(1.0))
-        self.beta = self.create_parameter((1, in_channels, 1, 1), dtype='float32', is_bias=True,
-        default_initializer=fluid.initializer.ConstantInitializer(0.0))
-        
-    def var(self, input, dim):
-        mean = fluid.layers.reduce_mean(input, dim, keep_dim=True)
-        tmp = fluid.layers.reduce_mean((input - mean)**2, dim, keep_dim=True)
-        return tmp
-
-    def forward(self, input):
-        in_mean, in_var = fluid.layers.reduce_mean(input, dim=[2,3], keep_dim=True), self.var(input, dim =[2,3])
-        out_in = (input - in_mean) / fluid.layers.sqrt(in_var + self.eps)
-        ln_mean, ln_var = fluid.layers.reduce_mean(input, dim=[1,2,3], keep_dim=True), self.var(input, dim=[1,2,3])
-        out_ln = (input - ln_mean) / fluid.layers.sqrt(ln_var + self.eps)
-        # ex_rho = fluid.layers.expand(self.rho, expand_times = [input.shape[0], 1, 1, 1])
-        # ex_gamma = fluid.layers.expand(self.gamma, expand_times = [input.shape[0], 1, 1, 1])
-        # ex_beta = fluid.layers.expand(self.beta, expand_times = [input.shape[0], 1, 1, 1])
-        # print("rho",fluid.layers.reduce_mean(self.rho).numpy())
-        # print("gamma",fluid.layers.reduce_mean(self.gamma).numpy())
-        # print("beta",fluid.layers.reduce_mean(self.beta).numpy())
-        # out = ex_rho * out_in + (1 - ex_rho) * out_ln
-        out = self.rho * out_in + (1 - self.rho)*out_ln
-        # out=out*fluid.layers.unsqueeze(fluid.layers.unsqueeze(self.gamma,2),3)+fluid.layers.unsqueeze(fluid.layers.unsqueeze(self.beta,2),3)
-        out = out * self.gamma + self.beta
-        return out
 
 
 class Discriminator(fluid.dygraph.Layer):
@@ -310,97 +218,3 @@ class Discriminator(fluid.dygraph.Layer):
         out = self.conv(x)
 
         return out, cam_logit, heatmap
-
-class Upsample(fluid.dygraph.Layer):
-    def __init__(self, scale=2):
-        super(Upsample, self).__init__()
-        self.scale = scale
-
-    def forward(self, inputs):
-        # reisze by actual_shape
-        out = fluid.layers.resize_nearest(
-            input=inputs, scale=self.scale)
-        return out
-
-class RhoClipper(object):
-    def __init__(self, min, max):
-        self.clip_min = min
-        self.clip_max = max
-        assert min < max
-
-    def __call__(self, module):
-
-        if hasattr(module, 'rho'):
-            w = module.rho.data
-            w = w.clamp(self.clip_min, self.clip_max)
-            module.rho.data = w
-
-class BCEWithLogitsLoss():
-    def __init__(self, weight=None, reduction='mean'):
-        self.weight = weight
-        self.reduction = 'mean'
-
-    def __call__(self, x, label):
-        out = fluid.layers.sigmoid_cross_entropy_with_logits(x, label)
-        if self.reduction == 'sum':
-            return fluid.layers.reduce_sum(out)
-        elif self.reduction == 'mean':
-            return fluid.layers.reduce_mean(out)
-        else:
-            return out
-        
-        
-class Spectralnorm(fluid.dygraph.Layer):
-    def __init__(self,
-                 layer,
-                 dim=0,
-                 power_iters=1,
-                 eps=1e-12,
-                 dtype='float32'):
-        super(Spectralnorm, self).__init__()
-        self.spectral_norm = nn.SpectralNorm(layer.weight.shape, dim, power_iters, eps, dtype)
-        self.dim = dim
-        self.power_iters = power_iters
-        self.eps = eps
-        self.layer = layer
-        weight = layer._parameters['weight']
-        del layer._parameters['weight']
-        self.weight_orig = self.create_parameter(weight.shape, dtype=weight.dtype)
-        self.weight_orig.set_value(weight)
-
-    def forward(self, x):
-        weight = self.spectral_norm(self.weight_orig)
-        self.layer.weight = weight
-        out = self.layer(x)
-        return out
-
-class ReflectionPad2d(fluid.dygraph.Layer):
-    def __init__(self, size):
-        super(ReflectionPad2d, self).__init__()
-        self.size = size
-
-    def forward(self, x):
-        return fluid.layers.pad2d(x, [self.size] * 4, mode="reflect")
-    
-        
-class ReLU(fluid.dygraph.Layer):
-    def __init__(self, inplace=False):
-        super(ReLU, self).__init__()
-        self.inplace=inplace
-
-    def forward(self, x):
-        if self.inplace:
-            x.set_value(fluid.layers.relu(x))
-            return x
-        else:
-            y=fluid.layers.relu(x)
-            return y
-        
-    
-class Leaky_ReLU(fluid.Layer):
-    def __init__(self, alpha=0.2):
-        super().__init__()
-        self.leaky_relu = lambda x: fluid.layers.leaky_relu(x, alpha=alpha)
-
-    def forward(self, x):
-        return self.leaky_relu(x)
---
+++ ops.py
@@ -0,0 +1,277 @@
+import paddle.fluid as fluid
+from paddle.fluid.layer_helper import LayerHelper
+from paddle.fluid.dygraph import Conv2D, Pool2D, BatchNorm, Linear,PRelu, SpectralNorm, LayerNorm, InstanceNorm
+from paddle.fluid.dygraph import Sequential
+import paddle.fluid.dygraph.nn as nn
+
+# def _get_default_param_initializer():
+#     filter_elem_num = filter_size[0] * filter_size[
+#         1] * self._num_channels
+#     std = (2.0 / filter_elem_num)**0.5
+#     return Normal(0.0, std, 0)
+
+# self.weight = self.create_parameter(
+#     attr=self._param_attr,
+#     shape=filter_shape,
+#     dtype=self._dtype,
+#     default_initializer=_get_default_param_initializer())
+
+# self.bias = self.create_parameter(
+#     attr=self._bias_attr,
+#     shape=[self._num_filters],
+#     dtype=self._dtype,
+#     is_bias=True)
+
+def init_w():
+    w_param_attrs = fluid.ParamAttr(
+                                initializer = fluid.initializer.NormalInitializer(loc=0.0,scale=0.2),
+                                learning_rate=0.0001,
+                                regularizer=fluid.regularizer.L2Decay(0.0001),
+                                trainable=True)
+    return w_param_attrs
+
+def init_bias(use_bias=True):
+    if use_bias:
+        bias_param_attrs = fluid.ParamAttr(
+                                    initializer = fluid.initializer.ConstantInitializer(value=0.0),
+                                    learning_rate=0.0001,
+                                    #regularizer=fluid.regularizer.L2Decay(0.0001),
+                                    trainable=True)
+    else:
+        bias_param_attrs = False
+
+    return bias_param_attrs
+
+
+class MLP(fluid.dygraph.Layer):
+    def __init__(self,in_nc=64,out_nc=64,light=True,use_bias=True):
+        super(MLP,self).__init__()
+                # ops for  Gamma, Beta block
+        self.light = light
+        
+        if self.light:
+            FC = [
+                  Linear(in_nc, out_nc,param_attr=init_w(),bias_attr=init_bias(use_bias),act='relu'),
+                  Linear(out_nc, out_nc,param_attr=init_w(),bias_attr=init_bias(use_bias),act='relu')
+                  ]
+        else:
+            FC = [
+                  Linear(out_nc, out_nc,param_attr=init_w(),bias_attr=init_bias(use_bias),act='relu'),
+                  Linear(out_nc, out_nc,param_attr=init_w(),bias_attr=init_bias(use_bias), act='relu')
+                  ]
+            
+        self.gamma = Linear(out_nc, out_nc,param_attr=init_w(),bias_attr=init_bias(use_bias))  # FC256
+        self.beta = Linear(out_nc, out_nc,param_attr=init_w(),bias_attr=init_bias(use_bias)) # FC256
+        self.FC = Sequential(*FC)
+  
+    def forward(self,x):
+        # alpha, beta
+        if self.light:
+            # 1/3,shape(N,256,64,64) -->(N,256,1,1)
+            x_ = fluid.layers.adaptive_pool2d(x, 1,pool_type='avg') # shape=(N,256,1,1)
+            x_ = fluid.layers.reshape(x_, shape=[x.shape[0], -1]) # shape=(N,256)  
+            # 2/3~3/3, x2 (N,256)-->(N,256) by FC256
+            x_ = self.FC(x_)
+        else:
+            # 1/3 (N,256,64,64)-->(N,256*64*64)
+            x_ = fluid.layers.reshape(x, shape=[x.shape[0], -1]) # shape=(N,256*64*64)
+            # 2/3 (N,64*64*256)-->(N,256), 2/3 (N,256)-->(N,256) by FC
+            x_ = self.FC(x_)
+        # (N,256)-->(N,256), parameters for AdaILN
+        gamma, beta = self.gamma(x_), self.beta(x_) # gamma torch.Size([N, 256]) beta torch.Size([N, 256])
+
+        return gamma,beta
+
+
+class ResnetBlock(fluid.dygraph.Layer):
+    def __init__(self, dim, use_bias=True):
+        super(ResnetBlock, self).__init__()
+        conv_block = []
+        conv_block += [ReflectionPad2d(1),
+                       Conv2D(dim, dim, filter_size=3, stride=1, padding=0, param_attr=init_w(),bias_attr=init_bias(use_bias)),
+                       InstanceNorm(dim),
+                       ReLU()                         
+                       ]
+
+        conv_block += [ReflectionPad2d(1),
+                       Conv2D(dim, dim, filter_size=3, stride=1, padding=0, param_attr=init_w(),bias_attr=init_bias(use_bias)),
+                       InstanceNorm(dim)
+                       ]
+
+        self.conv_block = Sequential(*conv_block)
+
+    def forward(self, x):
+        out = x + self.conv_block(x)
+        return out
+
+class ResnetAdaILNBlock(fluid.dygraph.Layer):
+    def __init__(self, dim, use_bias=True):
+        super(ResnetAdaILNBlock, self).__init__()
+        self.pad1 = ReflectionPad2d(1)
+        self.conv1 = Conv2D(dim, dim, filter_size=3, stride=1, padding=0, param_attr=init_w(),bias_attr=init_bias(use_bias))
+        self.norm1 = AdaILN(dim)
+        self.relu1 = ReLU()
+        self.pad2 = ReflectionPad2d(1)
+        self.conv2 = Conv2D(dim, dim, filter_size=3, stride=1, padding=0, param_attr=init_w(),bias_attr=init_bias(use_bias))
+        self.norm2 = AdaILN(dim)
+
+    def forward(self, x_init, gamma, beta):
+        x = self.pad1(x_init)
+        x = self.conv1(x)
+        x = self.norm1(x, gamma, beta)
+        x = self.relu1(x)
+        x = self.pad2(x)
+        x = self.conv2(x)
+        x = self.norm2(x, gamma, beta)
+        return  x + x_init
+
+
+class AdaILN(fluid.dygraph.Layer):
+
+    def __init__(self, in_channels, eps=1e-5):
+        super(AdaILN, self).__init__()
+        self.eps = eps
+        self.rho =self.create_parameter((1, in_channels, 1, 1), dtype='float32', is_bias=True,
+        default_initializer=fluid.initializer.ConstantInitializer(0.9))
+
+    def var(self, input, dim):
+        mean = fluid.layers.reduce_mean(input, dim, keep_dim=True)
+        tmp = fluid.layers.reduce_mean((input - mean)**2, dim, keep_dim=True)
+        return tmp
+
+    def forward(self, input, gamma, beta):
+        in_mean, in_var = fluid.layers.reduce_mean(input, dim=[2,3], keep_dim=True), self.var(input, dim =[2,3])
+        ln_mean, ln_var = fluid.layers.reduce_mean(input, dim=[1,2,3], keep_dim=True), self.var(input, dim=[1,2,3])
+        out_in = (input - in_mean) / fluid.layers.sqrt(in_var + self.eps)
+        out_ln = (input - ln_mean) / fluid.layers.sqrt(ln_var + self.eps)
+        out = self.rho * out_in + (1 - self.rho)*out_ln
+        out=out*fluid.layers.unsqueeze(gamma,[2,3])+fluid.layers.unsqueeze(beta,[2,3])
+        return out
+
+class LIN(fluid.dygraph.Layer):
+
+    def __init__(self, in_channels, eps=1e-5):
+        super(LIN, self).__init__()
+        self.eps = eps
+        self.rho = self.create_parameter((1, in_channels, 1, 1), dtype='float32', is_bias=True,
+        default_initializer=fluid.initializer.ConstantInitializer(1.0))  # 0.0 -- LN, 1.0 --IN
+        self.gamma = self.create_parameter((1, in_channels, 1, 1), dtype='float32', is_bias=True,
+        default_initializer=fluid.initializer.ConstantInitializer(1.0))
+        self.beta = self.create_parameter((1, in_channels, 1, 1), dtype='float32', is_bias=True,
+        default_initializer=fluid.initializer.ConstantInitializer(0.0))
+        
+    def var(self, input, dim):
+        mean = fluid.layers.reduce_mean(input, dim, keep_dim=True)
+        tmp = fluid.layers.reduce_mean((input - mean)**2, dim, keep_dim=True)
+        return tmp
+
+    def forward(self, input):
+        in_mean, in_var = fluid.layers.reduce_mean(input, dim=[2,3], keep_dim=True), self.var(input, dim =[2,3])
+        out_in = (input - in_mean) / fluid.layers.sqrt(in_var + self.eps)
+        ln_mean, ln_var = fluid.layers.reduce_mean(input, dim=[1,2,3], keep_dim=True), self.var(input, dim=[1,2,3])
+        out_ln = (input - ln_mean) / fluid.layers.sqrt(ln_var + self.eps)
+        # ex_rho = fluid.layers.expand(self.rho, expand_times = [input.shape[0], 1, 1, 1])
+        # ex_gamma = fluid.layers.expand(self.gamma, expand_times = [input.shape[0], 1, 1, 1])
+        # ex_beta = fluid.layers.expand(self.beta, expand_times = [input.shape[0], 1, 1, 1])
+        # print("rho",fluid.layers.reduce_mean(self.rho).numpy())
+        # print("gamma",fluid.layers.reduce_mean(self.gamma).numpy())
+        # print("beta",fluid.layers.reduce_mean(self.beta).numpy())
+        # out = ex_rho * out_in + (1 - ex_rho) * out_ln
+        out = self.rho * out_in + (1 - self.rho)*out_ln
+        # out=out*fluid.layers.unsqueeze(fluid.layers.unsqueeze(self.gamma,2),3)+fluid.layers.unsqueeze(fluid.layers.unsqueeze(self.beta,2),3)
+        out = out * self.gamma + self.beta
+        return out
+
+class Upsample(fluid.dygraph.Layer):
+    def __init__(self, scale=2):
+        super(Upsample, self).__init__()
+        self.scale = scale
+
+    def forward(self, inputs):
+        # reisze by actual_shape
+        out = fluid.layers.resize_nearest(
+            input=inputs, scale=self.scale)
+        return out
+
+class RhoClipper(object):
+    def __init__(self, min, max):
+        self.clip_min = min
+        self.clip_max = max
+        assert min < max
+
+    def __call__(self, module):
+
+        if hasattr(module, 'rho'):
+            w = module.rho.data
+            w = w.clamp(self.clip_min, self.clip_max)
+            module.rho.data = w
+
+class BCEWithLogitsLoss():
+    def __init__(self, weight=None, reduction='mean'):
+        self.weight = weight
+        self.reduction = 'mean'
+
+    def __call__(self, x, label):
+        out = fluid.layers.sigmoid_cross_entropy_with_logits(x, label)
+        if self.reduction == 'sum':
+            return fluid.layers.reduce_sum(out)
+        elif self.reduction == 'mean':
+            return fluid.layers.reduce_mean(out)
+        else:
+            return out
+        
+        
+class Spectralnorm(fluid.dygraph.Layer):
+    def __init__(self,
+                 layer,
+                 dim=0,
+                 power_iters=1,
+                 eps=1e-12,
+                 dtype='float32'):
+        super(Spectralnorm, self).__init__()
+        self.spectral_norm = nn.SpectralNorm(layer.weight.shape, dim, power_iters, eps, dtype)
+        self.dim = dim
+        self.power_iters = power_iters
+        self.eps = eps
+        self.layer = layer
+        weight = layer._parameters['weight']
+        del layer._parameters['weight']
+        self.weight_orig = self.create_parameter(weight.shape, dtype=weight.dtype)
+        self.weight_orig.set_value(weight)
+
+    def forward(self, x):
+        weight = self.spectral_norm(self.weight_orig)
+        self.layer.weight = weight
+        out = self.layer(x)
+        return out
+
+class ReflectionPad2d(fluid.dygraph.Layer):
+    def __init__(self, size):
+        super(ReflectionPad2d, self).__init__()
+        self.size = size
+
+    def forward(self, x):
+        return fluid.layers.pad2d(x, [self.size] * 4, mode="reflect")
+    
+        
+class ReLU(fluid.dygraph.Layer):
+    def __init__(self, inplace=False):
+        super(ReLU, self).__init__()
+        self.inplace=inplace
+
+    def forward(self, x):
+        if self.inplace:
+            x.set_value(fluid.layers.relu(x))
+            return x
+        else:
+            y=fluid.layers.relu(x)
+            return y
+        
+    
+class Leaky_ReLU(fluid.Layer):
+    def __init__(self, alpha=0.2):
+        super().__init__()
+        self.leaky_relu = lambda x: fluid.layers.leaky_relu(x, alpha=alpha)
+
+    def forward(self, x):
+        return self.leaky_relu(x)
