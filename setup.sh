8b5ed7b2859a72e025e64931b99e3a52252de341
Jay9z <jay.cn@outlook.com>
2020-09-07 02:47:38 +0800

    chore: add scoring, assets, and datasets template

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ python3 -m pip install paddlepaddle-gpu==1.8.2.post97 -i https://mirror.baidu.co
 python3 -m pip install paddlex -i https://mirror.baidu.com/pypi/simple;
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64;
 mkdir -p dataset/selfie2anime;
-unzip ~/data/data48778/308470_627400_bundle_archive.zip -d dataset/selfie2anime -y;
+unzip ~/data/data48778/308470_627400_bundle_archive.zip -d dataset/selfie2anime;
 #python main.py;
