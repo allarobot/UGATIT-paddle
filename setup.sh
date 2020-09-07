2148435e285a9abf74b08b3ddca5e847a8444912
Jay9z <jay.cn@outlook.com>
2020-09-07 19:56:51 +0800

    feat: script for aistudio

--- setup.sh
+++ setup.sh
@@ -4,4 +4,6 @@ python3 -m pip install paddlex -i https://mirror.baidu.com/pypi/simple;
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64;
 mkdir -p dataset/selfie2anime;
 unzip ~/data/data48778/308470_627400_bundle_archive.zip -d dataset/selfie2anime;
+mkdir -p results/selfie2anime/model
+cp -rf data/data52589/* results/selfie2anime/model/
 #python main.py;
