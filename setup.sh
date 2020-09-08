e11d58a7d856dd7725f954fbc833991c3cde25bb
Jay9z <jay.cn@outlook.com>
2020-09-08 15:21:55 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@ python3 -m pip install paddlepaddle-gpu==1.8.2.post97 -i https://mirror.baidu.co
 python3 -m pip install paddlex -i https://mirror.baidu.com/pypi/simple;
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64;
 mkdir -p dataset/selfie2anime;
+rm -rf dataset/selfie2anime/*;
 unzip ~/data/data48778/308470_627400_bundle_archive.zip -d dataset/selfie2anime;
 mkdir -p results/selfie2anime/model
 cp -rf data/data52589/* results/selfie2anime/model/
