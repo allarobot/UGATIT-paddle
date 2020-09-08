9c94dd8386a25a687c009ffec0ab800551f6005c
Jay9z <jay.cn@outlook.com>
2020-09-08 15:41:37 +0800

    Update resume_train.sh

--- resume_train.sh
+++ resume_train.sh
@@ -2,7 +2,7 @@
 export LD_LIBRARY_PATH=/usr/local/cuda/lib64;
 if [ $1 ]
 then
-    python main.py --phase train --light True --resume True --start_iteration $1 --iteration $2 --save_freq $3;
+    python main.py --phase train --light True --resume True  --iteration $1 --save_freq $2 --start_iteration $3;
 else
     python main.py --phase train --light True --resume True --iteration 100000 --save_freq 10000 --start_iteration 45000;
 fi
